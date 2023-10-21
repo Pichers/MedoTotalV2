2bcf1ce68c7eb6dddcbc2609bfa804ce0b647788
Antonio Almeida <fc58235@alunos.fc.ul.pt>
2023-10-21 15:03:24 +0100

    Override of actions and falha_antecipada for pergunta1

--- MedoTotalTurbo.py
+++ MedoTotalTurbo.py
@@ -9,8 +9,8 @@ class MedoTotalTurbo(MedoTotal):
         super().__init__(texto_input)
         self.distPast = {} #p = pastilha xy[][] = matriz com a distância de cada (x,y) a p
         ps = self.initial[1]
-        for i in range(len(ps)):
-            self.distPast[ps[i]] = self.distanciasP(self, ps[i])
+        for p in ps:
+            self.distPast[p] = self.distanciasP(self, p)
 
     def distanciasP(self, p):
         d = self.dim
@@ -78,5 +78,32 @@ class MedoTotalTurbo(MedoTotal):
             ((x,y) != self.fantasma) and
             ((x,y) not in visited)):
 
+            return True
+        return False
+    
+    def actions(self, state):
+        x, y = state.pacman
+        return [act for act in self.directions.keys() 
+                if (x+self.directions[act][0],y+self.directions[act][1]) not in (self.obstacles | {self.fantasma}) and 
+                not self.falha_antecipada(self.result(state,act))]
+    
+    def falha_antecipada(self,state):
+        pacman = state[0]
+
+        if state.tempo <= state.medo:
+            return False
+        if state.pastilhas == set(): # se não há mais pastilhas e eram necessárias
+            return True
+        
+        #minDist = min(list(map(lambda x: manhatan(state.pacman,x),state.pastilhas)))
+        dists = []
+        for p in state[1]:
+            dists.append(self.distPast[p][pacman[0]][pacman[1]])
+        minDist = min(dists)
+
+        if minDist > state.medo: # se não há tempo (manhatan) para chegar à próxima super-pastilha
+            return True
+        if (state.medo + self.poder * len(state.pastilhas)) < state.tempo:
+            # se o poder de todas as pastilhas mais o medo são insuficientes.
             return True
         return False

9e9698c3a06f97b33942bcac53e6a2101f34cb9b
Pedro <fc58212@alunos.fc.ul.pt>
2023-10-22 04:24:35 +0100

    teste v2

--- ex2.py
+++ ex2.py
@@ -17,24 +17,17 @@ def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
     beststate = None
 
     while stack:
-        node = stack.pop()
+        node = stack.popleft()
         state = node.state
         if len(stack) > max_mem:
             max_mem = len(stack)
-            
-        
         if optimal and beststate is not None and node.path_cost >= beststate.path_cost:
-                continue
-            
+            continue
         if verbose:
             print("---------------------")
             print()
             print(problem.display(state))
-            
-        
-        # if state not in visited: 
-
-            
+    
         if problem.goal_test(state):
             estados_finais.append(node)
             if beststate is None or node.path_cost < beststate.path_cost:
@@ -44,37 +37,22 @@ def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
                     print("Di best goal até agora")
                     visited.add(state)
                     continue
-                    
-                    
             visited.add(state)
-
             if verbose:
                 if node in estados_finais:
                     print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
                 else:
                     print("Custo:", node.path_cost)
             continue
-        
-        
-        
-        
         if verbose:
             print("Custo:", node.path_cost)
-            
-            
-            
+        
         for i, action in enumerate(problem.actions(state)):
-            child_state = problem.result(state, action)
-            child = Node(child_state, node, action, problem.path_cost(node.path_cost, state, action, child_state))
+            child = Node(problem.result(state, action), node, action, problem.path_cost(node.path_cost, state, action, problem.result(state, action)))
             if optimal:
-                # if beststate is not None:
-                #     print("child.path_cost:", child.path_cost, "beststate.path_cost:", beststate.path_cost)
-                if beststate is not None and child.path_cost >= beststate.path_cost:
+                if (beststate is not None and child.path_cost >= beststate.path_cost) or (beststate is not None and node.path_cost >= beststate.path_cost):
                     continue
             stack.insert(i, child)
-
-
-
 
         visited.add(state)
             

787e1d98061916761ddfbf1600567edea288e35c
Pedro <fc58212@alunos.fc.ul.pt>
2023-10-22 05:25:43 +0100

    merda e mais merda

--- ex2.py
+++ ex2.py
@@ -12,49 +12,152 @@ def depth_first_tree_search_all_count(problem, optimal=False, verbose=False):
     stack.append(n1)
     visited.add(n1.state)
     max_mem = 0
-
     estados_finais = []
     beststate = None
 
-    while stack:
+    while(stack):
         node = stack.popleft()
         state = node.state
         if len(stack) > max_mem:
             max_mem = len(stack)
-        if optimal and beststate is not None and node.path_cost >= beststate.path_cost:
-            continue
+            
+        if optimal:
+            if beststate is not None and node.path_cost >= beststate.path_cost:
+                continue
+            if verbose:
+                print("---------------------")
+                print()
+                print(problem.display(state))
+            if problem.goal_test(state):
+                if beststate is None or node.path_cost < beststate.path_cost:
+                    estados_finais.append(node)
+                    beststate = node
+                    visited.add(state)
+                    if verbose:
+                        print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
+                        print("Di best goal até agora")
+                else:
+                    if verbose:
+                        if node in estados_finais:
+                            print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
+                        else:
+                            print("Custo:", node.path_cost)
+                continue
+            else:
+                if verbose:
+                    print("Custo:", node.path_cost)
+                if beststate is not None and node.path_cost >= beststate.path_cost:
+                    continue
+            visited.add(state)
+            for i, action in enumerate(problem.actions(state)):
+                child = Node(problem.result(state, action), node, action, problem.path_cost(node.path_cost, state, action, problem.result(state, action)))
+                if( beststate is not None and child.path_cost >= beststate.path_cost):
+                    continue
+                stack.insert(i, child)
+            continue  
+            
+            
         if verbose:
             print("---------------------")
             print()
             print(problem.display(state))
-    
+            visited.add(state)
+            if problem.goal_test(state):
+                estados_finais.append(node)
+                if beststate is None or node.path_cost < beststate.path_cost:
+                    beststate = node
+                    print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
+                    print("Di best goal até agora")
+                else:
+                    if node in estados_finais:
+                        print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
+                    else:
+                        print("Custo:", node.path_cost)
+                continue
+            else:
+                print("Custo:", node.path_cost)
+            for i, action in enumerate(problem.actions(state)):
+                child = Node(problem.result(state, action), node, action, problem.path_cost(node.path_cost, state, action, problem.result(state, action)))
+                stack.insert(i, child) 
+            continue
+        
+
+
+            # normal
+        visited.add(state)
         if problem.goal_test(state):
             estados_finais.append(node)
             if beststate is None or node.path_cost < beststate.path_cost:
                 beststate = node
-                if verbose:
-                    print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
-                    print("Di best goal até agora")
-                    visited.add(state)
-                    continue
-            visited.add(state)
-            if verbose:
-                if node in estados_finais:
-                    print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
-                else:
-                    print("Custo:", node.path_cost)
             continue
-        if verbose:
-            print("Custo:", node.path_cost)
-        
         for i, action in enumerate(problem.actions(state)):
             child = Node(problem.result(state, action), node, action, problem.path_cost(node.path_cost, state, action, problem.result(state, action)))
-            if optimal:
-                if (beststate is not None and child.path_cost >= beststate.path_cost) or (beststate is not None and node.path_cost >= beststate.path_cost):
-                    continue
             stack.insert(i, child)
-
+            visited.add(problem.result(state, action))
         visited.add(state)
-            
 
     return beststate, max_mem, len(visited), len(estados_finais)
+
+def teste():
+    parametros="T=15\nM=6\nP=10"
+    linha1= "= = = = = =\n"
+    linha2= "= * F @ * =\n"
+    linha3= "= . . . . =\n"
+    linha4= "= . = . . =\n"
+    linha5= "= . = . . =\n"
+    linha6= "= = = = = =\n"
+    grelha=linha1+linha2+linha3+linha4+linha5+linha6
+    mundoStandard2=parametros + "\n" + grelha
+    gx=MedoTotal(mundoStandard2)
+    resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx)
+    print('*'*20)
+    if resultado:
+        print("Solução Prof-total (árvore) com custo " + str(resultado.path_cost)+":")
+        print(resultado.solution())
+    else:
+        print('\nSem Solução')
+    print('Visitados=',visitados)
+    print('Dimensão máxima da memória',max_mem)
+    print('Estados finais:',finais)
+
+
+    # while stack:
+    #     node = stack.popleft()
+    #     state = node.state
+    #     if len(stack) > max_mem:
+    #         max_mem = len(stack)
+    #     if optimal and beststate is not None and node.path_cost >= beststate.path_cost:
+    #         continue
+    #     if verbose:
+    #         print("---------------------")
+    #         print()
+    #         print(problem.display(state))
+    
+    #     if problem.goal_test(state):
+    #         estados_finais.append(node)
+    #         if beststate is None or node.path_cost < beststate.path_cost:
+    #             beststate = node
+    #             if verbose:
+    #                 print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
+    #                 print("Di best goal até agora")
+    #                 visited.add(state)
+    #                 continue
+    #         visited.add(state)
+    #         if verbose:
+    #             if node in estados_finais:
+    #                 print("GGGGooooooallllll --------- com o custo:" ,node.path_cost)
+    #             else:
+    #                 print("Custo:", node.path_cost)
+    #         continue
+    #     if verbose:
+    #         print("Custo:", node.path_cost)
+        
+    #     for i, action in enumerate(problem.actions(state)):
+    #         child = Node(problem.result(state, action), node, action, problem.path_cost(node.path_cost, state, action, problem.result(state, action)))
+    #         if optimal:
+    #             if (beststate is not None and child.path_cost >= beststate.path_cost) or (beststate is not None and node.path_cost >= beststate.path_cost):
+    #                 continue
+    #         stack.insert(i, child)
+
+    #     visited.add(state)
+            

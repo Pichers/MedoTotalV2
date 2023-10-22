de6333c9fda34fdcbb1087be49b41ccb47ba4ee0
Antonio Almeida <fc58235@alunos.fc.ul.pt>
2023-10-22 13:48:33 +0100

    trying to bug fix

--- testes.py
+++ testes.py
@@ -1,3 +1,27 @@
+def teste():
+    parametros="T=15\nM=6\nP=10"
+    linha1= "= = = = = =\n"
+    linha2= "= * F @ * =\n"
+    linha3= "= . . . . =\n"
+    linha4= "= . = . . =\n"
+    linha5= "= . = . . =\n"
+    linha6= "= = = = = =\n"
+    grelha=linha1+linha2+linha3+linha4+linha5+linha6
+    mundoStandard2=parametros + "\n" + grelha
+    gx=MedoTotal(mundoStandard2)
+    resultado,max_mem,visitados,finais = depth_first_tree_search_all_count(gx)
+    print('*'*20)
+    if resultado:
+        print("Solução Prof-total (árvore) com custo " + str(resultado.path_cost)+":")
+        print(resultado.solution())
+    else:
+        print('\nSem Solução')
+    print('Visitados=',visitados)
+    print('Dimensão máxima da memória',max_mem)
+    print('Estados finais:',finais)
+
+#---------------------------------------------------------------------------------------------------------------------
+
 s = ProblemaGrafo()
 print('---------------- IDA* ----------------')
 f=lambda n: n.path_cost + s.h1(n)
